857a53fec77be39e562415b23bbf8218d48d412f
kic0412 <kic0412@naver.com>
2023-05-22 16:42:17 +0900

    05-22

---
+++ javaJazz/wearetheone/src/ch04/String4Demo.java
@@ -0,0 +1,10 @@
+package ch04;
+
+public class String4Demo {
+    public static void main(String[] args) {
+        int i = 7;
+        System.out.println("Java" + i);
+        System.out.println("Java" + 7);
+        System.out.println(7 + 1 + "Java" + 7 + 1);
+    }
+}
---
+++ javaJazz/wearetheone/src/ch04/String5Demo.java
@@ -0,0 +1,14 @@
+package ch04;
+
+public class String5Demo {
+    public static void main(String[] args) {
+        String version = String.format("%s %d", "JDK", 14);
+        System.out.println(version);
+
+        String fruits = String.join(",", "apple", "banana", "cherry", "durian");
+        System.out.println(fruits);
+
+        String pi = String.valueOf(3.14);
+        System.out.println(pi);
+    }
+}
