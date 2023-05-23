5a67eb8237531b2e6dcf5e1c9acb893229292bc9
kic0412 <kic0412@naver.com>
2023-05-23 14:32:40 +0900

    05-23

--- javaJazz/wearetheone/src/ch06/GirlTest.java
+++ javaJazz/wearetheone/src/ch06/GirlTest.java
@@ -1,10 +1,7 @@
 package ch06;
 
 class Girl {
-    private String name;
-
-    Girl() {
-    }
+    protected String name;
 
     Girl(String name) {
         this.name = name;
@@ -16,24 +13,30 @@ class Girl {
 }
 
 class GoodGirl extends Girl {
+
+    GoodGirl(String name) {
+        super(name);
+    }
     void show() {
-        System.out.println("그녀는 자바를 잘 안다.");
+        System.out.println(name + "는 자바를 잘 안다.");
     }
 }
 
 class BestGirl extends GoodGirl {
+
+    BestGirl(String name) {
+        super(name);
+    }
     void show() {
-        System.out.println("그녀는 자바를 무지하게 잘 안다.");
+        System.out.println(name + "는 자바를 무지하게 잘 안다.");
     }
 }
 
 public class GirlTest {
     public static void main(String[] args) {
-        Girl g1 = new Girl();
-        GoodGirl g2 = new GoodGirl();
-        GoodGirl gg = new BestGirl();
+        Girl[] girls = {new Girl("갑순이"), new GoodGirl("콩쥐"), new BestGirl("김경목")};
 
-        g2.show();
-        gg.show();
+        for (Girl g : girls)
+            g.show();
     }
 }
