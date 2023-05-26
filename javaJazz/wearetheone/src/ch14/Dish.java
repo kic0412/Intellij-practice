3c91dd9102f017f1827f467199f2dfa70b1f89e1
kic0412 <kic0412@naver.com>
2023-05-26 17:56:08 +0900

    05-26

---
+++ javaJazz/wearetheone/src/ch14/Dish.java
@@ -0,0 +1,13 @@
+package ch14;
+
+public class Dish {
+    private boolean empty = true;
+
+    public boolean isEmpt() {
+        return empty;
+    }
+
+    public void setEmpty(boolean empty) {
+        this.empty = empty;
+    }
+}
