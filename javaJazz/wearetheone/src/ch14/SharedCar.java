037046c0dee7cb1c897f399692f7b6aa08a2d029
kic0412 <kic0412@naver.com>
2023-05-26 17:42:42 +0900

    05-26

---
+++ javaJazz/wearetheone/src/ch14/SharedCar.java
@@ -0,0 +1,13 @@
+package ch14;
+
+import java.util.Random;
+
+public class SharedCar {
+    public synchronized void drive(String name, String where) {
+        System.out.println(name + "님이 자동차를 탔습니다.");
+        Random r = new Random();
+        for (int i = 0; i < r.nextInt(3)+ 1 ; i++)
+            System.out.println(name + "님이 자동차를 운전합니다.");
+        System.out.println(name + "님이" + where + "에 도착했습니다.");
+    }
+}
